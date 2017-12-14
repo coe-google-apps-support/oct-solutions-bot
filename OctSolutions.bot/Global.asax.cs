7f0268e86173858841084033dbeb8fc9dc8abd2d
Dan Chenier <daniel.chenier@edmonton.ca>
2017-12-14 11:29:13 -0700

    Checkpoint

--- OctSolutions.bot/Global.asax.cs
+++ OctSolutions.bot/Global.asax.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using OctSolutions.bot.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace OctSolutions.bot
@@ -12,6 +15,11 @@ namespace OctSolutions.bot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
---
+++ OctSolutions.bot/Models/HomeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OctSolutions.bot.Models
+{
+    public class HomeModel
+    {
+        public string SearchText { get; set; }
+    }
+}
